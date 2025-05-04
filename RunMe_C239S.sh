925476d80de0f637936c66f0ed2a707368b7fc4d
mckeownish <j.mckeown124@gmail.com>
2025-05-04 14:03:15 +0100

    Run fits for paper test cases, code for reconstruction

--- RunMe_C239S.sh
+++ RunMe_C239S.sh
@@ -3,7 +3,7 @@
 ROOT=$(dirname "$(readlink -f "$0")")
 
 # Directory to clear before running
-CLEAR_DIR="$ROOT/carbonara_runs/SMARCAL1_repro/fitdata"
+CLEAR_DIR="$ROOT/carbonara_runs/C239S/fitdata"
 
 # Clear the directory
 echo "Clearing directory: $CLEAR_DIR"
@@ -11,15 +11,15 @@ rm -r "$CLEAR_DIR"/*
 mkdir -p "$CLEAR_DIR"
 
 ### argv[ 1] scattering data file
-ScatterFile=$ROOT/carbonara_runs/SMARCAL1_repro/Saxs.dat
+ScatterFile=$ROOT/carbonara_runs/C239S/Saxs.dat
 ### argv[ 2] sequence file location
-fileLocs=$ROOT/carbonara_runs/SMARCAL1_repro/
+fileLocs=$ROOT/carbonara_runs/C239S/
 ### argv[ 3] restart tag (use to start from existing prediction)
 initialCoordsFile=frompdb
 ### argv[ 4] paired distances file (can be empty)
-pairedPredictions=$ROOT/carbonara_runs/SMARCAL1_repro/fixedDistanceConstraints1.dat
+pairedPredictions=$ROOT/carbonara_runs/C239S/fixedDistanceConstraints1.dat
 ### argv[ 5] fixed sections file (again can be empty)
-fixedsections=$ROOT/carbonara_runs/SMARCAL1_repro/varyingSectionSecondary1.dat
+fixedsections=$ROOT/carbonara_runs/C239S/varyingSectionSecondary1.dat
 ### argv[ 6] number of structures
 noStructures=1
 ### argv[ 7] request to apply hydrophobic covering WITHIN monomers will be a list of sections on which to apply it -- Currently not used
@@ -33,19 +33,19 @@ kmaxStart=0.1
 ### argv[11] Max number of fitting steps
 maxNoFitSteps=1000
 ### argv[12] prediction file - mol[i] in the fitting folder
-predictionFile=$ROOT/carbonara_runs/SMARCAL1_repro/fitdata
+predictionFile=$ROOT/carbonara_runs/C239S/fitdata
 ### argv[13] scattering output file
-scatterOut=$ROOT/carbonara_runs/SMARCAL1_repro/fitdata
+scatterOut=$ROOT/carbonara_runs/C239S/fitdata
 ### argv[14] mixture list file, alist of sets of numbers indicatig the allowed set of mixture percentages of each species (e.g. dimer 20 monomer 80)
-mixtureFile=$ROOT/carbonara_runs/SMARCAL1_repro/mixtureFile.dat
+mixtureFile=$ROOT/carbonara_runs/C239S/mixtureFile.dat
 ### argv[15] previous fit string in form fitname/mol6Substep_10_1.dat+fitname/mol6Substep_10_2.dat
-prevFitStr=$ROOT/carbonara_runs/SMARCAL1_repro/redundant
+prevFitStr=$ROOT/carbonara_runs/C239S/redundant
 ### argv[16] log file location
-logLoc=$ROOT/carbonara_runs/SMARCAL1_repro/fitdata
+logLoc=$ROOT/carbonara_runs/C239S/fitdata
 ### argv[17] last line of the previous fit log, this is only used for a restart if argv[3] = True
 endLinePrevLog=null
 ### argv[18] is true if we want to apply affine rotations,false if not.
-affineTrans=False
+affineTrans=True
 for i in {1..5}
 do
     echo "\n"
