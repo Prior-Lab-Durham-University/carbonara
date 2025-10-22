4bb4fa836ac653bcb51a33d11d4ffd0876cbc7e9
ktch24 <60183644+ktch24@users.noreply.github.com>
2025-10-22 18:40:42 +0100

    Update experimentalData.cpp

--- src/experimentalData.cpp
+++ src/experimentalData.cpp
@@ -64,8 +64,8 @@ experimentalData::experimentalData(const char* scatterFile){
     
     // set up "idealised q values for Josh's coefficients (to change maybe)"
 
-    for (int i = 0; i <= 20; i++) {
-        q.push_back(0.01 * i);
+    for (int i = 0; i < 20; i++) {
+        q.push_back(0.2/19.0 * i);
     }
     
 }
